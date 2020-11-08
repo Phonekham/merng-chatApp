9f9297dad98d8474d6d5481fdf6b646903e6e8c8
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-11-08 11:31:57 +0700

    Messages context

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { AuthProvider } from "./context/auth";
+import { MessageProvider } from "./context/message";
 import DymnamicRoute from "./utils/DynamicRoute";
 
 import ApolloProvider from "./ApolloProvider";
@@ -14,15 +15,17 @@ function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <Container className="pt-5">
-            <Switch>
-              <DymnamicRoute exact path="/" component={Home} authenticated />
-              <DymnamicRoute path="/register" component={Register} />
-              <DymnamicRoute path="/login" component={Login} />
-            </Switch>
-          </Container>
-        </BrowserRouter>
+        <MessageProvider>
+          <BrowserRouter>
+            <Container className="pt-5">
+              <Switch>
+                <DymnamicRoute exact path="/" component={Home} authenticated />
+                <DymnamicRoute path="/register" component={Register} />
+                <DymnamicRoute path="/login" component={Login} />
+              </Switch>
+            </Container>
+          </BrowserRouter>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
   );
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,89 +1,17 @@
-import { useEffect, useState } from "react";
-import { Button, Col, Image, Row } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { gql, useLazyQuery, useQuery } from "@apollo/client";
 
 import { useAuthDispatch } from "../../context/auth";
-
-const GET_USERS = gql`
-  query getUsers {
-    getUsers {
-      username
-      createdAt
-      imageUrl
-      latestMessage {
-        uuid
-        content
-        from
-        to
-        createdAt
-      }
-    }
-  }
-`;
-
-const GET_MESSAGES = gql`
-  query getMessages($from: String!) {
-    getMessages(from: $from) {
-      uuid
-      from
-      to
-      content
-      createdAt
-    }
-  }
-`;
+import Users from "./Users";
+import Messages from "./Messages";
 
 const Home = ({ history }) => {
   const dispatch = useAuthDispatch();
-  const [selectedUser, setSelectedUser] = useState(null);
-
-  const { loading, data, error } = useQuery(GET_USERS);
-  const [
-    getMessages,
-    { loading: messagesLoading, data: messagesData },
-  ] = useLazyQuery(GET_MESSAGES);
-
-  useEffect(() => {
-    if (selectedUser) {
-      getMessages({ variables: { from: selectedUser } });
-    }
-  }, [selectedUser]);
 
   const logout = () => {
     dispatch({ type: "LOGOUT" });
     history.push("/login");
   };
-
-  let usersMarkup;
-  if (!data || loading) {
-    usersMarkup = <p>Loading...</p>;
-  } else if (data.getUsers.length === 0) {
-    usersMarkup = <p>no user join yet</p>;
-  } else if (data.getUsers.length > 0) {
-    usersMarkup = data.getUsers.map((user) => (
-      <div
-        className="d-flex p-3"
-        key={user.username}
-        onClick={() => setSelectedUser(user.username)}
-      >
-        <Image
-          src={user.imageUrl}
-          roundedCircle
-          className="mr-2"
-          style={{ width: 50, height: 50, objectFit: "cover" }}
-        />
-        <div>
-          <p className="text-success">{user.username}</p>
-          <p className="font-weight-light">
-            {user.latestMessage
-              ? user.latestMessage.content
-              : "You are now connected!"}
-          </p>
-        </div>
-      </div>
-    ));
-  }
 
   return (
     <>
@@ -99,16 +27,8 @@ const Home = ({ history }) => {
         </Button>
       </Row>
       <Row className="bg-white mt-2">
-        <Col xs={4}>{usersMarkup}</Col>
-        <Col xs={8}>
-          {messagesData && messagesData.getMessages.length > 0 ? (
-            messagesData.getMessages.map((message) => (
-              <p key={message.uuid}>{message.content}</p>
-            ))
-          ) : (
-            <p>Messages</p>
-          )}
-        </Col>
+        <Users />
+        <Messages />
       </Row>
     </>
   );
