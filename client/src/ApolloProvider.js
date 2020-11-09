e62014e3f9a1ca8efbf276f3c109aba0afa8ad06
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-11-09 12:04:02 +0700

    receive message & subscription

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -3,17 +3,18 @@ import {
   InMemoryCache,
   ApolloProvider as Provider,
   createHttpLink,
+  split,
 } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { setContext } from "@apollo/client/link/context";
 
-const httpLink = createHttpLink({
+let httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -22,8 +23,32 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+httpLink = authLink.concat(httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
--- graphql/resolvers/messages.js
+++ graphql/resolvers/messages.js
@@ -1,4 +1,8 @@
-const { UserInputError, AuthenticationError } = require("apollo-server");
+const {
+  UserInputError,
+  AuthenticationError,
+  withFilter,
+} = require("apollo-server");
 const { Op } = require("sequelize");
 
 const { User, Message } = require("../../models");
@@ -32,7 +36,7 @@ module.exports = {
     },
   },
   Mutation: {
-    sendMessage: async (_, { to, content }, { user }, info) => {
+    sendMessage: async (_, { to, content }, { user, pubsub }, info) => {
       if (!user) throw new AuthenticationError("Unauthenticated");
       const recipient = await User.findOne({ where: { username: to } });
       if (!recipient) {
@@ -46,7 +50,26 @@ module.exports = {
         to,
         content,
       });
+      pubsub.publish("NEW_MESSAGE", { newMessage: message });
       return message;
     }, //End SendMessage
+  },
+  Subscription: {
+    newMessage: {
+      subscribe: withFilter(
+        (_, __, { pubsub, user }) => {
+          if (!user) throw new AuthenticationError("Unauthenticated");
+          return pubsub.asyncIterator(["NEW_MESSAGE"]);
+        },
+        ({ newMessage }, _, { user }) => {
+          if (
+            newMessage.from === user.username ||
+            newMessage.to === user.username
+          ) {
+            return true;
+          }
+        }
+      ),
+    },
   },
 };
